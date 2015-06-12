99d9a89c9819394e5f7efe11ca21f6c390e52cb3
bobafett0 <craumermike@yahoo.com>
2015-06-12 18:59:05 -0400

    Create Sucker.java

---
+++ Sucker.java
@@ -0,0 +1,12 @@
+
+public class Sucker implements IFunction  
+{
+
+	@Override
+	public int function(double[][] payoff, int[][] moveHistory) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
